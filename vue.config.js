05c2f38fef032c1380b321bbf5b222a65e654744
Alex Oxthorn <zamerick@gmail.com>
2018-11-24 00:42:01 -0500

    Setup
    Deleted boilerplate, added tailwind/purgecss and a few other configuration tasks

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+class TailwindExtractor {
+  static extract (content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [] // eslint-disable-line no-useless-escape
+  }
+}
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './../src/index.html'),
+          path.join(__dirname, './../**/*.vue'),
+          path.join(__dirname, './../src/**/*.js')
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ['html', 'js', 'php', 'vue']
+          }
+        ],
+        whitelist: ['background']
+      })
+    ]
+  }
+}
